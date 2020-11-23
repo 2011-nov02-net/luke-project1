e115ee6ced7299c971c7bac8ce80760884ad7056
Luke Fisher <thebigfishluke@gmail.com>
2020-11-23 10:38:24 -0500

    basic project structure added

---
+++ StoreApplication.WebApp/StoreApplicationMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreApplicationMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
